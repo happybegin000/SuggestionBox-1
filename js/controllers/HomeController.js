164134d4fdbeefb636366d564f74279c70faeb29
Steve Toro <steve@stevetoro.com>
2016-12-05 09:03:14 -0500

    Form to add new suggestions.

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -3,5 +3,19 @@ app.controller('HomeController', [
   'suggestions',
   ($scope, suggestions) => {
     $scope.posts = suggestions.posts;
+
+    $scope.addSuggestion = () => {
+      if ($scope.title.trim() !== "") { 
+        let suggestion = {
+          title: $scope.title,
+          upvotes: 0,
+          comments: []
+        };
+
+        $scope.posts.push(suggestion);
+        $scope.title = "";
+      };
+    };
+
   }
 ]);
